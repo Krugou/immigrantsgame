fea344fcdc1fe83186fcfc00b6d25b9edbdb0b25
Krugou <59641229+Krugou@users.noreply.github.com>
2023-05-17 21:04:40 +0300

    Update MainView.js

--- views/MainView.js
+++ views/MainView.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
 
 const MainView = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome to the Main View!</Text>
-        </View>
-    );
-};
+  const [count, setCount] = useState(0);
+  const [autoIncrementers, setAutoIncrementers] = useState(0);
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    },
-});
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count => count + autoIncrementers);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [autoIncrementers]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Count: {count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)} />
+      <Button title="Buy Auto Incrementer" onPress={() => setAutoIncrementers(autoIncrementers + 1)} />
+    </View>
+  );
+};
 
 export default MainView;
